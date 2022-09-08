ae863aec281ad7a092e25786d8ee27ed982f0fc5
TemmaMukai <tenma34@icloud.com>
2022-09-08 11:04:32 +0900

    paizaの練習問題

---
+++ paiza/src/paiza1/Main.java
@@ -0,0 +1,15 @@
+package paiza1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt();
+		for (int i = 0; i < s; i ++) {
+			System.out.println("paiza");
+		}
+	}
+
+}
